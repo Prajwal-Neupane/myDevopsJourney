cb0dc49940895f5265f3232fb475e48820738bdf
Prajwal Neupane <prajwalneupane.981@gmail.com>
2023-05-10 20:34:30 +0545

    day 2 completed

--- Golang/src/main.go
+++ Golang/src/main.go
@@ -1,24 +1,47 @@
 package main
 
-import "fmt" //format package
+import (
+	"fmt" //format package
+)
 
 
 
 
 func main() {
-	var i int
 
+    // var firstName string
+	// var lastName string
+	// var userAge int
+	// var email string
+	// fmt.Println("Enter your first name: ")
+	// fmt.Scan(&firstName)
+	// fmt.Println("Enter your second name: ")
+	// fmt.Scan(&lastName)
+	// fmt.Println("Enter your email: ")
+	// fmt.Scan(&email)
+	// fmt.Printf("Enter your age: ")
+	// fmt.Scan(&userAge)
+	// fmt.Printf("Hello %v %v !! Your email is %v and age is %v", firstName,lastName, email, userAge)
 
-	fmt.Print("Type a number: ")
-	fmt.Scan(&i)
-	fmt.Println("Your number is: ", i)
-	userInput()
+	var userNumber int
+	fmt.Println("Enter number of users you want: ")
+	fmt.Scan(&userNumber)
+
+	 allUsers:= []string{}
+
+	for i := 0; i < userNumber; i++ {
+		fmt.Printf("Enter %v user full name	: ", i+1)
+		fmt.Scan(&allUsers[i])
+		
+	}
+
+	fmt.Println(allUsers)
+	
+	
+
+	
+
+	
+	
 }
-func userInput() {
 
-	var name string
-	fmt.Print("Enter your name: ")
-	fmt.Scan(name)
-	fmt.Println("Hello", name, "! Welcome to our website")
-
-}
