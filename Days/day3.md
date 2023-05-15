0aa10a6eb19f8fa7a620ef56680c3c6d969a4975
Prajwal Neupane <prajwalneupane.981@gmail.com>
2023-05-15 18:22:38 +0545

    conditions and other stuffs

---
+++ Days/day3.md
@@ -0,0 +1,12 @@
+Today is day 3. Today I"m gonna finish the golang course by TechWorld with Nana.
+
+
+We can also have a unused variable and we can denote it by _
+
+Today I learne about loops, if else in golang which are pretty similar with c and c++ programming which I have already learned. 
+
+Another way to write for loop in golang is 
+
+for someCondition > 0 && len(someCondition) < 50 {
+    <!-- Execute this block of code -->
+}
--- Golang/src/main.go
+++ Golang/src/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt" //format package
+	// "strings"
 )
 
 
@@ -23,19 +24,74 @@ func main() {
 	// fmt.Scan(&userAge)
 	// fmt.Printf("Hello %v %v !! Your email is %v and age is %v", firstName,lastName, email, userAge)
 
-	var userNumber int
-	fmt.Println("Enter number of users you want: ")
-	fmt.Scan(&userNumber)
+	// var userNumber int
+	// fmt.Println("Enter number of users you want: ")
+	// fmt.Scan(&userNumber)
 
-	 allUsers:= []string{}
+	//  allUsers:= []string{}
 
-	for i := 0; i < userNumber; i++ {
-		fmt.Printf("Enter %v user full name	: ", i+1)
-		fmt.Scan(&allUsers[i])
+	// for i := 0; i < userNumber; i++ {
+	// 	fmt.Printf("Enter %v user full name	: ", i+1)
+	// 	fmt.Scan(&allUsers[i])
+		
+	// }
+
+	// To check for the valid user FirstName, LastName and Email
+	// validUser := len(firstName) >= 2 && len(lastName) >= 2
+	// validEmail := strings.Contains(email, "@")
+
+
+	// if validUser && validEmail {
+	// 	fmt.Println("User Input Data is True")
+		
+	// } else {
+	// 	fmt.Println("The statement is not true")
+	// }
+
+
+	
+	
+	// for i := 1; i <= 5; i++ {
+	// 	for j := 1; j <=i ; j++ {
+	// 		fmt.Printf("*")
+			
+	// 	}
+	// 	fmt.Printf("\n")
+		
+	// }
+	
+
+	// fmt.Println(allUsers)
+
+	var a int
+	var b int
+	var c string
+
+	fmt.Println("Enter the value of a: ")
+	fmt.Scan(&a)
+	fmt.Println("Enter the value of b: ")
+	fmt.Scan(&b)
+	fmt.Println("Enter the operator: +, -, *, /  : ")
+	fmt.Scan(&c)
+
+	switch c {
+	case "+":
+		fmt.Printf("The sum of %v and %v is %v", a, b, a+b)
+		
+	case "-":
+		fmt.Printf("The difference of %v and %v is %v", a, b, a-b)
+		
+	case "*":
+		fmt.Printf("The product of %v and %v is %v", a, b, a*b)
+		
+	case "/":
+		fmt.Printf("The quotient of %v and %v is %v", a, b, a/b)
+		
+	 default:
+		fmt.Printf("Error in selecting operator")
+
 		
 	}
-
-	fmt.Println(allUsers)
 	
 	
 
